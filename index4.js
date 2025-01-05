3dc26547dc14dec0092948d779b3738bc56872c7
erooms <erooms2024@gmail.com>
2025-01-05 19:56:33 +0200

    Add files via upload

---
+++ index4.js
@@ -0,0 +1,37 @@
+import express from "express";
+import bodyParser from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+
+const app = express();
+const port = 3000;
+
+
+function logger(req, res, next){
+  console.log("Request method: ", req.method);
+  console.log("Request url: ", req.url);
+  next();
+}
+app.use(logger);
+
+
+app.get("/", (req, res) => {
+ // console.log(__dirname + "/public/index.html");
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.use(bodyParser.urlencoded({ extended: false}));
+app.post("/submit", (req, res)=> {
+     console.log(req.body['street']);
+     res.send( `<h1>Your band name is</h1> <h2>${req.body['street']} +${req.body['pet']} </h2>`);
+});
+
+
+
+
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
